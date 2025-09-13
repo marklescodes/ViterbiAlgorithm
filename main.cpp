31da12629f9c1d55990ebcef1935d735e260bdbf
marklescodes <lolakov22@gmail.com>
2025-09-13 18:46:45 +0300

    Initialise project structure

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+#include <limits>
+#include <ctime>
+
+using namespace std;
+
+int main(); {
+
+
+    return 0;
+}
+

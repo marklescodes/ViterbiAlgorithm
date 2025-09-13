59e7bdf6c20cdc7a1df86430494c368aeb3d75fe
marklescodes <lolakov22@gmail.com>
2025-09-13 19:02:04 +0300

    Add and coinfigure class for convolutional coder

--- main.cpp
+++ main.cpp
@@ -7,7 +7,39 @@
 
 using namespace std;
 
-int main(); {
+class ConvolutionalCode {
+public:
+    int convlength;
+    vector<int> polynoms;
+
+    ConvolutionalCode(int k, vector<int> p):
+    convlength(k), polynoms(p) {}
+
+    vector<int> code(const vector<int> &bits) {
+        vector <int> output;
+        vector <int> shift(convlength, 0);
+
+        for (size_t n = 0; n < bits.size(); n++) {
+            for (int i = convlength - 1; i >0 ; i--)
+                shift[i] = shift[i-1];
+            shift[0] = bits[n];
+        }
+
+        for (size_t g = 0; g < polynoms.size(); g++) {
+            int gen = polynoms[g];
+            int val = 0;
+            for (int i = 0; i < convlength; i++) {
+                if ((gen >> i) & 1) val ^= shift[i];
+            }
+        }
+
+        return output;
+    }
+};
+
+
+
+int main() {
 
 
     return 0;

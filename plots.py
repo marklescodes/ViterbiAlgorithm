170060d4aaf00dd7ab4b904946738b42a76aebf4
marklescodes <lolakov22@gmail.com>
2025-09-14 22:52:43 +0300

    Create plots.py

---
+++ plots.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import csv
+
+x, y = [], []
+
+with open("ber_vs_error.csv") as f:
+    reader = csv.reader(f, delimiter=";")
+    for row in reader:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+
+plt.plot(x, y, marker='o')
+plt.xlabel("Channel Error Probability")
+plt.ylabel("Bit Error Rate (BER)")
+plt.title("BER vs Channel Error Probability")
+plt.grid(True)
+plt.show()
